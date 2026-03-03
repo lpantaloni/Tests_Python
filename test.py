b2581a0f4971d663efcfd70b6cf55d0e6ef6b137
lpantaloni <laurent.pantaloni@mac.com>
2026-03-03 08:32:43 +0100

    First commit message & Sync

---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+
+print("Welcome to the Python programming world!")
+pipo = os.getlogin()
+print(f"Hello, {pipo}!")
+print("Welcome to the world of Python programming!")
+print("This is a simple script that greets the user by their login name.")
+print("Feel free to modify this script and add more functionality!")
+print("Happy coding!")
